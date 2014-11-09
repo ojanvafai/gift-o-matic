9cba1553c149978a32ba2f0ac2b90ff47113ef65
Julie Parent <jparent@gmail.com>
2014-11-08 16:46:58 -0800

    Refactor NewItemForm
    Creates ItemForm and separates out form creation from NewItemForm.

---
+++ ui/ItemForm.js
@@ -0,0 +1,33 @@
+function getValue(ref) {
+  return ref.getDOMNode().value.trim();
+}
+
+var ItemForm = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  handleOpen: function(event) {
+    this.setState({expanded: true});
+  },
+  handleSave: function(event) {
+    this.props.onAdd(
+        getValue(this.refs.owner),
+        getValue(this.refs.quantity),
+        getValue(this.refs.title),
+        getValue(this.refs.link),
+        getValue(this.refs.description));
+    this.props.onSave();
+  },
+  render: function() {
+    return <div className="ItemForm">
+      <div className="ItemFormInputs">
+        <div>For: <input ref="owner" /></div>
+        <div>Count: <input ref="quantity" /></div>
+        <div>Item: <input ref="title" /></div>
+      </div>
+      <div className="flex">Link: <input className="flexOne" ref="link" /></div>
+      <div>Notes: <textarea ref="description" /></div>
+      <div><button onClick={this.handleSave}>Save</button></div>
+    </div>;
+  },
+});
--- ui/NewItemForm.js
+++ ui/NewItemForm.js
@@ -1,7 +1,3 @@
-function getValue(ref) {
-  return ref.getDOMNode().value.trim();
-}
-
 var NewItemForm = React.createClass({
   getInitialState: function() {
     return {};
@@ -10,28 +6,12 @@ var NewItemForm = React.createClass({
     this.setState({expanded: true});
   },
   handleSave: function(event) {
-    this.props.onAdd(
-        getValue(this.refs.owner),
-        getValue(this.refs.quantity),
-        getValue(this.refs.title),
-        getValue(this.refs.link),
-        getValue(this.refs.description));
     this.setState({expanded: false})
   },
   render: function() {
     if (!this.state.expanded) {
       return <div onClick={this.handleOpen}>Click here to add a new item</div>;
     }
-
-    return <div className="newItemForm">
-      <div className="newItemFormInputs">
-        <div>For: <input ref="owner" /></div>
-        <div>Count: <input ref="quantity" /></div>
-        <div>Item: <input ref="title" /></div>
-      </div>
-      <div className="flex">Link: <input className="flexOne" ref="link" /></div>
-      <div>Notes: <textarea ref="description" /></div>
-      <div><button onClick={this.handleSave}>Save</button></div>
-    </div>;
+    return <ItemForm onSave={this.handleSave} onAdd={this.props.onAdd}/>
   },
 });
