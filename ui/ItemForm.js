66f1a76a77e3f86831d8a7159f89b1618c37be6c
Ojan Vafai <ojan@chromium.org>
2014-11-09 15:43:19 -0800

    Add support for two recipients.

--- ui/ItemForm.js
+++ ui/ItemForm.js
@@ -2,30 +2,46 @@ function getValue(ref) {
   return ref.getDOMNode().value.trim();
 }
 
+function pushIfNonEmpty(array, ref) {
+  var value = getValue(ref);
+  if (value)
+    array.push(value);
+}
+
 var ItemForm = React.createClass({
   getInitialState: function() {
     return {};
   },
   setValue: function(valStr) {
-    if (this.props.data) {
-      if (this.props.data[valStr])
-        this.refs[valStr].getDOMNode().value = this.props.data[valStr];
-    }
+    if (this.props.data[valStr])
+      this.refs[valStr].getDOMNode().value = this.props.data[valStr];
   },
   handleOpen: function(event) {
     this.setState({expanded: true});
-    this.setValue('recipients');
+    if (!this.props.data)
+      return;
+
     this.setValue('quantity');
     this.setValue('description');
     this.setValue('links');
     this.setValue('notes');
+
+    var recipients = this.props.data.recipients;
+    this.refs.recipient1.getDOMNode().value = recipients[0];
+    // TODO: Support an arbitrary number of recipients.
+    if (recipients.length > 1)
+      this.refs.recipient2.getDOMNode().value = recipients[1];
   },
   handleSave: function(event) {
-    // TODO: Support multiple recipients
     var key = this.props.data && this.props.data.key;
+
+    var recipients = [];
+    pushIfNonEmpty(recipients, this.refs.recipient1);
+    pushIfNonEmpty(recipients, this.refs.recipient2);
+
     this.props.onSaveItem({
       key: key,
-      recipients: [getValue(this.refs.recipients)],
+      recipients: recipients,
       quantity: getValue(this.refs.quantity),
       description: getValue(this.refs.description),
       links: [getValue(this.refs.links)],
@@ -39,7 +55,7 @@ var ItemForm = React.createClass({
   render: function() {
     return <div className="ItemForm">
       <div className="ItemFormInputs">
-        <div>For: <input ref="recipients"/></div>
+        <div>For: <input ref="recipient1"/> &amp; <input ref="recipient2"/></div>
         <div>Count: <input ref="quantity" /></div>
         <div>Item: <input ref="description" /></div>
       </div>
